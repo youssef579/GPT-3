6361b14ff4cf87558c7be05a6e9875df2f810da9
Youssef Ragaey <youssefragaa3@gmail.com>
2023-02-17 23:34:29 +0200

    Finish banner section

--- src/components/children/Feature.jsx
+++
@@ -1,8 +0,0 @@
-export default function Feature({ heading, paragraph }) {
-    return (
-        <article className="feature">
-            <h3 className="gradient-before extra-bold">{heading}</h3>
-            <p className="blue">{paragraph}</p>
-        </article>
-    );
-}
